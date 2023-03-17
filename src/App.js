7f46caa1c5f8921db8679c559c3e564b1a02e986
Jasbir Singh <jasbir212singh@gmail.com>
2023-03-17 09:29:34 +0530

    react query loading and error state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
+import { isError, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
 function App() {
-  console.log(useQuery);
+  //const queryClient = useQueryClient();
+  // const queryInfo =  useQuery(['pokemon'] , () => {
+  //   axios.get('https://pokeapi.co/api/v2/pokemon').then(res => res.data.results)
+  // })
+
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.data),
+  });
+
+  //if (isLoading) return <h1>Loading</h1>
+  
   return (
     <div className="App">
       <h1>Hello React Query</h1>
+      {data?.results?.length &&
+        data.results.map((item, idx) => {
+          return <h4 key={idx}>{item?.name}</h4>;
+        })}
     </div>
   );
 }
